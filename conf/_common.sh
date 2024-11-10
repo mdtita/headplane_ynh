f0c27065992dadf33359edd6d85e237289889055
Mohamed Tita <hi@mtita.me>
2024-11-10 11:41:17 +0100

    fix20

--- conf/_common.sh
+++ conf/_common.sh
@@ -7,7 +7,13 @@ nodejs_version=22
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
+check_headscale_user() {
+    ynh_system_user_exists --username=headscale
+    if [ $? -eq 1 ]; then
+        ynh_die "Headscale app is not installed Aborting."
+    fi
+    # Continue the script if user exists
+}
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
